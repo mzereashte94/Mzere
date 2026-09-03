9266cf9fba5806ae647ebf2b7db035f19a2a13c5
mzereashte94 <mzereashte03@gmail.com>
2026-09-04 02:19:44 +0300

    Update sign.js

--- api/sign.js
+++ api/sign.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
-    const { app, password, p12, prov, use_free_cert } = req.body;
+    const { app, password, p12, prov, use_free_cert, cert_id } = req.body;
     const GITHUB_TOKEN = process.env.GITHUB_PAT;
     const GITHUB_REPO = process.env.GITHUB_REPO;
     const buildId = Date.now().toString();
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
                     p12_base64: p12 || "",
                     prov_base64: prov || "",
                     use_free_cert: use_free_cert ? "true" : "false",
+                    cert_id: cert_id || "china", // لێرەدا ئایدی شهادەکە دەنێردرێت
                     build_id: buildId
                 }
             })
@@ -29,13 +30,11 @@ export default async function handler(req, res) {
 
         if (!githubRes.ok) {
             const errorText = await githubRes.text();
-            console.error("GitHub API Error:", errorText);
             return res.status(500).json({ error: 'Engine rejected the request', details: errorText });
         }
         
         res.status(200).json({ build_id: buildId });
     } catch (error) {
-        console.error("Signing API Error:", error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
